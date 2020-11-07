6a158a5e99bceedb979a3c56514c6762a3779280
isido <shido322@snu.ac.kr>
2020-11-07 22:58:40 +0900

    [feat] add picture saving logic - add addOnUpdateListener with onSceneUpdate as callback - onSceneUpdate will update recent Image - add onClick logic onSavePicture - it write image as file with the current Image updated by onSceneUpdate - add toastMsg if image saved successfully

--- app/src/main/java/com/journaldev/androidarcoredistancecamera/MainActivity.java
+++ app/src/main/java/com/journaldev/androidarcoredistancecamera/MainActivity.java
@@ -1,11 +1,18 @@
 package com.journaldev.androidarcoredistancecamera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -14,9 +21,12 @@ import android.widget.Toast;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.Frame;
 import com.google.ar.core.Pose;
+import com.google.ar.core.exceptions.NotYetAvailableException;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.SceneView;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.MaterialFactory;
@@ -25,6 +35,15 @@ import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements Scene.OnUpdateListener {
@@ -40,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     private Anchor firstAnchor = null;
     private Anchor secondAnchor = null;
     private AnchorNode secondAnchorNode;
+    private Image mCurrentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +73,11 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
         tvDistance = findViewById(R.id.tvDistance);
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener((this::onSceneUpdate));
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
 
 
         initModel();
@@ -157,7 +182,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     public void onUpdate(FrameTime frameTime) {
         Frame frame = arFragment.getArSceneView().getArFrame();
 
-        Log.d("API123", "onUpdateframe... current anchor node " + (firstAnchorNode == null));
+       // Log.d("API123", "onUpdateframe... current anchor node " + (firstAnchorNode == null));
 
 
         if (firstAnchorNode != null && secondAnchorNode != null) {
@@ -185,5 +210,89 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
         //https://stackoverflow.com/questions/48191513/how-to-take-picture-with-camera-using-arcore
         float ab = 1;
 
+        try {
+            WriteImageInformation(mCurrentImage, generateFilename());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        toastMsg("saved image successfully");
+
     }
+
+    private void onSceneUpdate(FrameTime frameTime) {
+        Frame currentFrame = arFragment.getArSceneView().getArFrame();
+        if (mCurrentImage != null) {
+            mCurrentImage.close();
+            mCurrentImage = null;
+        }
+        try {
+            mCurrentImage = currentFrame.acquireCameraImage();
+        } catch (NotYetAvailableException e) {
+            e.printStackTrace();
+        }
+        if (mCurrentImage != null) {
+            int imageFormat = mCurrentImage.getFormat();
+            if (imageFormat == ImageFormat.YUV_420_888) {
+                Log.d("ImageFormat", "Image format is YUV_420_888");
+            }
+        }
+    }
+
+    private static byte[] NV21toJPEG(byte[] nv21, int width, int height) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        YuvImage yuv = new YuvImage(nv21, ImageFormat.NV21, width, height, null);
+        yuv.compressToJpeg(new Rect(0, 0, width, height), 100, out);
+        return out.toByteArray();
+    }
+
+    public static void WriteImageInformation(Image image, String path) throws IOException {
+        byte[] data = null;
+        data = NV21toJPEG(YUV_420_888toNV21(image),
+                image.getWidth(), image.getHeight());
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
+        bos.write(data);
+        bos.flush();
+        bos.close();
+    }
+
+    private static byte[] YUV_420_888toNV21(Image image) {
+        byte[] nv21;
+        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
+        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer();
+        ByteBuffer vBuffer = image.getPlanes()[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        nv21 = new byte[ySize + uSize + vSize];
+
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        return nv21;
+    }
+
+    private String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+
+        File directory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/");
+        if (!directory.exists()) {
+            boolean retVal = directory.mkdirs();
+            Log.d("generateFileName", "directory... " + (retVal));
+        }
+
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/" + date + "_screenshot.jpg";
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
 }
