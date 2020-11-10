5c8753735facaef727f26a64c19e9ca7c3fdfc53
isido <shido322@snu.ac.kr>
2020-11-11 00:19:15 +0900

    [refactor] replace store image logic - acquireCameraImage does not show all view hierarchy . It shows only surface view content - rendering image of dots to measure distance can't be captured with the acquireCameraImage. - so replaced rather using pixelcopy.request, this copies every pixel which is same as preview image of the ar fragment

--- app/src/main/java/com/journaldev/androidarcoredistancecamera/MainActivity.java
+++ app/src/main/java/com/journaldev/androidarcoredistancecamera/MainActivity.java
@@ -5,13 +5,13 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.graphics.ImageFormat;
-import android.graphics.Rect;
-import android.graphics.YuvImage;
-import android.media.Image;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
+import android.view.PixelCopy;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,12 +35,10 @@ import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -132,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
         btnRecord.setOnClickListener(v->{
             try {
-                StoreImage(generateFilename());
+                takeScreenshot(generateFilename());
                 toastMsg("saved image successfully");
             } catch (IOException | NotYetAvailableException e) {
                 e.printStackTrace();
@@ -186,43 +184,6 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     private boolean IsDistanceMeasured() {
         return firstAnchorNode != null && secondAnchorNode != null;
     }
-    private void StoreImage(String path) throws IOException, NotYetAvailableException {
-        byte[] data;
-        Image image = arSceneView.getArFrame().acquireCameraImage();
-        data = NV21toJPEG(YUV_420_888toNV21(image),
-                image.getWidth(), image.getHeight());
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
-        bos.write(data);
-        bos.flush();
-        bos.close();
-    }
-
-    private static byte[] NV21toJPEG(byte[] nv21, int width, int height) {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        YuvImage yuv = new YuvImage(nv21, ImageFormat.NV21, width, height, null);
-        yuv.compressToJpeg(new Rect(0, 0, width, height), 100, out);
-        return out.toByteArray();
-    }
-
-    private static byte[] YUV_420_888toNV21(Image image) {
-        byte[] nv21;
-        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
-        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer();
-        ByteBuffer vBuffer = image.getPlanes()[2].getBuffer();
-
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
-
-        nv21 = new byte[ySize + uSize + vSize];
-
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
-
-        return nv21;
-    }
 
     private String generateFilename() {
         String date =
@@ -236,6 +197,30 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
         return Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/" + date + "_screenshot.jpg";
+    }
+
+    private void takeScreenshot(String path) throws IOException, NotYetAvailableException {
+        Bitmap captureBitmap = Bitmap.createBitmap(arSceneView.getWidth(), arSceneView.getHeight(), Bitmap.Config.ARGB_8888);
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        PixelCopy.request(arSceneView, captureBitmap, new PixelCopy.OnPixelCopyFinishedListener() {
+            @Override
+            public void onPixelCopyFinished(int copyResult) {
+                if (copyResult == PixelCopy.SUCCESS && captureBitmap != null) {
+                    try {
+                        FileOutputStream outputStream = new FileOutputStream(path);
+                        ByteArrayOutputStream outputData = new ByteArrayOutputStream();
+                        captureBitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputData);
+                        outputData.writeTo(outputStream);
+                        outputStream.flush();
+                        outputStream.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                handlerThread.quitSafely();
+            }
+        }, new Handler(handlerThread.getLooper()));
     }
 
     private void toastMsg(String msg) {
