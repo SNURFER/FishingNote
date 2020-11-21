04287efd624fb88e5fdec24bce940d82fd40030d
ca1207 <ktyca0320>
2020-11-21 23:24:25 +0900

    convention change + insert function changed

--- app/src/main/java/com/journaldev/androidarcoredistancecamera/DbHandler.java
+++ app/src/main/java/com/journaldev/androidarcoredistancecamera/DbHandler.java
@@ -8,14 +8,14 @@ import android.database.sqlite.SQLiteStatement;
 import java.util.Vector;
 
 public class DbHandler {
-    private Context context = null;
-    private SQLiteDatabase database = null;
+    private Context m_context = null;
+    private SQLiteDatabase m_database = null;
 
     public DbHandler(Context context) {
-        this.context = context;
-        this.database = this.context.openOrCreateDatabase("FISHDB",
+        this.m_context = context;
+        this.m_database = this.m_context.openOrCreateDatabase("FISHDB",
                 Context.MODE_PRIVATE, null);
-        CreateTables();
+        createTables();
     }
     //Table Memory Structures
     public static class FishInfo {
@@ -26,56 +26,57 @@ public class DbHandler {
     }
     //Public methods
 
-    public void InsertInToFishInfo(int user_id, String name, float size, byte[] image) {
-        SQLiteStatement insert_query = this.database.compileStatement("INSERT INTO FISH_INFO " +
+    public void insertInToFishInfo(int user_id, String name, float size, byte[] image, String date,
+                                   float altitude, float latitude) {
+        SQLiteStatement insertQuery = this.m_database.compileStatement("INSERT INTO FISH_INFO " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?);");
-        insert_query.bindDouble(1, user_id);
-        insert_query.bindString(2, name);
-        insert_query.bindDouble(3, size);
-        insert_query.bindBlob(4, image);
-        insert_query.bindDouble(5, 0);
-        insert_query.bindDouble(6, 0);
-        insert_query.bindDouble(7, 0);
-        insert_query.executeInsert();
+        insertQuery.bindDouble(1, user_id);
+        insertQuery.bindString(2, name);
+        insertQuery.bindDouble(3, size);
+        insertQuery.bindBlob(4, image);
+        insertQuery.bindString(5, date);
+        insertQuery.bindDouble(6, altitude);
+        insertQuery.bindDouble(7, latitude);
+        insertQuery.executeInsert();
     }
 
-    public void Delete (){
-        String delete_query_ = "Delete From FISH_INFO";
-        this.database.execSQL(delete_query_);
+    public void delete () {
+        String deleteQuery = "Delete From FISH_INFO";
+        this.m_database.execSQL(deleteQuery);
     }
 
-    public Vector<FishInfo> SelectFromFishInfo(String name) {
+    public Vector<FishInfo> selectFromFishInfo(String name) {
         Cursor csr;
-        String select_query = "SELECT * FROM FISH_INFO";
+        String selectQuery = "SELECT * FROM FISH_INFO";
         if (name != null) {
-            select_query = select_query + " WHERE NAME = ?";
-            csr = this.database.rawQuery(select_query, new String[]{name});
+            selectQuery = selectQuery + " WHERE NAME = ?";
+            csr = this.m_database.rawQuery(selectQuery, new String[]{name});
         } else {
-            csr = this.database.rawQuery(select_query,null);
+            csr = this.m_database.rawQuery(selectQuery,null);
         }
-        return ConvertToFishInfos(csr);
+        return convertToFishInfos(csr);
     }
     //Private methods
-    private void CreateTables() {
-        String create_fish_info = "CREATE TABLE IF NOT EXISTS FISH_INFO (user_id INTEGER, " +
+    private void createTables() {
+        String createFishInfo = "CREATE TABLE IF NOT EXISTS FISH_INFO (user_id INTEGER, " +
                 " name TEXT, size REAL, image BLOB, date TEXT, altitude REAL, latitude REAL);";
-        this.database.execSQL(create_fish_info);
+        this.m_database.execSQL(createFishInfo);
     }
 
-    private Vector<FishInfo> ConvertToFishInfos(Cursor csr) {
-        Vector<FishInfo> fish_infos = new Vector<>();
+    private Vector<FishInfo> convertToFishInfos(Cursor csr) {
+        Vector<FishInfo> fishInfos = new Vector<>();
 
         if (!csr.moveToFirst())
-            return fish_infos;
+            return fishInfos;
         while(!csr.isAfterLast()) {
-            FishInfo fish_info = new FishInfo();
-            fish_info.user_id = csr.getInt(csr.getColumnIndex("user_id"));
-            fish_info.name = csr.getString(csr.getColumnIndex("name"));
-            fish_info.size = csr.getFloat(csr.getColumnIndex("size"));
-            fish_info.image = csr.getBlob(csr.getColumnIndex("image"));
-            fish_infos.add(fish_info);
+            FishInfo fishInfo = new FishInfo();
+            fishInfo.user_id = csr.getInt(csr.getColumnIndex("user_id"));
+            fishInfo.name = csr.getString(csr.getColumnIndex("name"));
+            fishInfo.size = csr.getFloat(csr.getColumnIndex("size"));
+            fishInfo.image = csr.getBlob(csr.getColumnIndex("image"));
+            fishInfos.add(fishInfo);
             csr.moveToNext();
         }
-        return fish_infos;
+        return fishInfos;
     }
 }
